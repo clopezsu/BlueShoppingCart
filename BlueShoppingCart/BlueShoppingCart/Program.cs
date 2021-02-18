4064430c4288f229bd5f5350f9471a847c881a02
Cynthia Lopez <>
2021-02-17 23:42:58 -0700

    This is the first commit

---
+++ BlueShoppingCart/BlueShoppingCart/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BlueShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            int y = 1001;
+            int n = 4;
+            int t = 9;
+
+            y++;
+
+            c = y + n * t;
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
